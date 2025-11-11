c2890cff2caacba1f9484e014b03148ceb4415d3
nasko22346 <nasko.nik.22@gmail.com>
2025-11-11 09:28:29 +0200

    Initial commit

---
+++ src/test/java/org/informatics/logistics_company/LogisticsCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.informatics.logistics_company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsCompanyApplicationTests {
+
+	//@Test
+	//void contextLoads() {
+	//}
+
+}
