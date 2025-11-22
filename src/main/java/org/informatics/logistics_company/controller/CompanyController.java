6ac388f75b41a22ab9a3da3b5ec175b902b1aaa5
Pepi Zlatev <f116665@students.nbu.bg>
2025-11-22 18:11:53 +0200

    [initial-structure] add basic getmapping

--- src/main/java/org/informatics/logistics_company/controller/CompanyController.java
+++ src/main/java/org/informatics/logistics_company/controller/CompanyController.java
@@ -1,11 +1,15 @@
 package org.informatics.logistics_company.controller;
 
 import org.informatics.logistics_company.service.CompanyService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping("/companies")
+@RequestMapping("/api/v1/companies")
 public class CompanyController {
 
     private final CompanyService companyService;
@@ -14,6 +18,10 @@ public class CompanyController {
         this.companyService = companyService;
     }
 
+    @GetMapping("/load")
+    public List<?> loadCompanies() {
+        return new ArrayList<>();
+    }
 
     //TODO: Implement controller methods - Get, Post, Put, Delete
 }
