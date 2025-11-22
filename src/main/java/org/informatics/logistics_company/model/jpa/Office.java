1ecd7748cd434558bbab00cc1d362dce4a3bda8f
paskalev2001 <2001paskalpaskalev@gmail.com>
2025-11-22 18:24:37 +0200

    Company Entity Model implementation (todo 1-1 loc; n-1 openTime)

---
+++ src/main/java/org/informatics/logistics_company/model/jpa/Office.java
@@ -0,0 +1,30 @@
+package org.informatics.logistics_company.model.jpa;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "office")
+@Data
+public class Office {
+    @Id
+    @Column(name = "office_id")
+    private Long officeId;
+
+    // To be removed according to the Location implementation
+//    @Column(name = "office_address")
+//    private String officeAddress;
+
+    @Column(name = "office_phone")
+    private String officePhone;
+
+    @Column(name = "office_email")
+    private String officeEmail;
+
+    @ManyToOne
+    private Company company;
+
+    // Todo implement One to One relation for Location
+    // Todo implement Many to One relation for OpenTime
+}
+
