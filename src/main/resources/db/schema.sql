9739c3014b0e555a974ad0f8ed2f93cfbe24d68c
Pepi Zlatev <f116665@students.nbu.bg>
2025-11-22 17:23:41 +0200

    [initial-structure] remove schema.sql file as it is not needed

--- src/main/resources/db/schema.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists companies
-(
-    company_id          int          not null auto_increment primary key,
-    company_name        varchar(255) not null,
-    phone_number        varchar(50) not null,
-    email               varchar(255) not null unique,
-    company_eik         varchar(50) not null,
-    company_description text
-) engine=InnoDB default charset=utf8mb4;
