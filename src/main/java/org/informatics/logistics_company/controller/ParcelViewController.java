e1cde9233c4ab65e6ef73a7877e57ce66de7b306
pzlatev <pepi.zlatev@it-economics.com>
2026-01-06 23:06:09 +0200

    [add-thymeleaf] Add home page and page for clients parcel. Modify the ParcelViewController. Extract common style into style.css

--- src/main/java/org/informatics/logistics_company/controller/ParcelViewController.java
+++ src/main/java/org/informatics/logistics_company/controller/ParcelViewController.java
@@ -3,7 +3,9 @@ package org.informatics.logistics_company.controller;
 import org.informatics.logistics_company.service.ParcelService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +22,12 @@ public class ParcelViewController {
     public String getAllParcels(Model model) {
         model.addAttribute("parcels", parcelService.fetchAllParcels());
         return "parcels";
+    }
+
+    @GetMapping("/client/{clientId}")
+    public String getClientParcels(@PathVariable Long clientId, Model model) {
+        model.addAttribute("parcels", parcelService.fetchParcelsForClient(clientId));
+        model.addAttribute("clientId", clientId);
+        return "client_parcels";
     }
 }
--- src/main/java/org/informatics/logistics_company/dto/parcel/ParcelRequest.java
+++ src/main/java/org/informatics/logistics_company/dto/parcel/ParcelRequest.java
@@ -7,30 +7,29 @@ import lombok.Data;
 @Data
 public class ParcelRequest {
 
-    @NotNull(message = "Weight cannot be null")
+//    @NotNull(message = "Weight cannot be null")
     @Positive(message = "Weight must be positive")
     private Double weight;
 
-    @NotNull(message = "SenderID cannot be null")
+//    @NotNull(message = "SenderID cannot be null")
     @Positive(message = "SenderID must be positive")
     private Long senderId;
 
-    @NotNull(message = "ReceiverID cannot be null")
+//    @NotNull(message = "ReceiverID cannot be null")
     @Positive(message = "ReceiverID must be positive")
     private Long receiverId;
 
-    @NotNull(message = "RegisteredByStaffID cannot be null")
+//    @NotNull(message = "RegisteredByStaffID cannot be null")
     @Positive(message = "RegisteredByStaffID must be positive")
     private Long registeredByStaffId;
 
-    @NotNull(message = "SentLocationID cannot be null")
+//    @NotNull(message = "SentLocationID cannot be null")
     @Positive(message = "SentLocationID must be positive")
     private Long sentLocationId;
 
-    @NotNull(message = "ReceivedLocationID cannot be null")
+//    @NotNull(message = "ReceivedLocationID cannot be null")
     @Positive(message = "ReceivedLocationID must be positive")
     private Long receivedLocationId;
-
 
     private boolean deliverToAddress;
 }
